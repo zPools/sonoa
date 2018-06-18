97bbbdb76db66d66d46393400ede36f6257430d3
Mike <zpoolsde@gmail.com>
2018-06-18 13:24:21 +0200

    RPC update

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -119,13 +119,13 @@ Value getworkex(const Array& params, bool fHelp)
             "getworkex [data, coinbase]\n"
             "If [data, coinbase] is not specified, returns extended work data.\n"
         );
-/*
+
     if (vNodes.empty())
         throw JSONRPCError(-9, "SONO is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(-10, "SONO is downloading blocks...");
-*/
+
     if (pindexBest->nHeight >= LAST_POW_BLOCK)
         throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
 
@@ -253,13 +253,13 @@ Value getwork(const Array& params, bool fHelp)
             "  \"hash1\" : formatted hash buffer for second hash (DEPRECATED)\n" // deprecated
             "  \"target\" : little endian hash target\n"
             "If [data] is specified, tries to solve the block and returns true if it was successful.");
-/*
+
     if (vNodes.empty())
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "SONO is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "SONO is downloading blocks...");
-*/
+
     if (pindexBest->nHeight >= LAST_POW_BLOCK)
         throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
 
@@ -401,13 +401,13 @@ Value getblocktemplate(const Array& params, bool fHelp)
 
     if (strMode != "template")
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
-/*
+
     if (vNodes.empty())
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "SONO is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "SONO is downloading blocks...");
-*/
+
     if (pindexBest->nHeight >= LAST_POW_BLOCK)
         throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
 
