54392efcef9dab369b86735aed74ec1db03372ea
Mike <31688573+zPools@users.noreply.github.com>
2018-07-08 15:24:40 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 20001;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 20012;
+static const int PROTOCOL_VERSION = 20021;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 20011;
