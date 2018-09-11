f25a43565d39f1498aec2672702c833f5d41e201
Mike <31688573+zPools@users.noreply.github.com>
2018-09-11 13:23:06 +0200

    changed server
    Maybe a smarter idea to keep the load of my pool :P

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -20,8 +20,8 @@
 #define DECORATION_SIZE 64
 #define NUM_ITEMS 3
 
-const QString BaseURL = "https://new.zPools.de/SONOusd.php";
-const QString BaseURL2 = "https://new.zPools.de/SONObtc.php";
+const QString BaseURL = "http://multi.zPools.de/SONOusd.php";
+const QString BaseURL2 = "http://multi.zPools.de/SONObtc.php";
 double sonox;
 double SONObtcx;
 
