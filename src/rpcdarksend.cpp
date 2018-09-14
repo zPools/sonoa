57f093c73c604b1091f3d09364ba6e3549802e5b
zPools <zpoolsde@gmail.com>
2018-09-14 08:59:59 +0200

    fasthash build fix

--- src/rpcdarksend.cpp
+++ src/rpcdarksend.cpp
@@ -228,14 +228,14 @@ Value masternode(const Array& params, bool fHelp)
 
             if(strCommand == "active"){
                 obj.push_back(Pair(mn.addr.ToString().c_str(), (string)(mn.IsEnabled() ? "Enabled" : "Error") ));
-            } else if (strCommand == "full") {                                              //Reports "MasternodeIP: Enabled, Last time seen, Time active, Rank, Protocol"
+            } else if (strCommand == "full") {                                                  //Reports "MasternodeIP: Enabled, Last time seen, Time active, Rank, Protocol"
                 std::ostringstream streamFull;
                 streamFull << std::setw(18) <<
-                              (string)(mn.IsEnabled() ? "Enabled" : "Error") << " " <<      // Is Enabled
-                              (int64_t)mn.lastTimeSeen << " " <<                            // Last time seen
-                              (int64_t)(mn.lastTimeSeen - mn.now) << " " <<                 // Time active (in sec)
-                              (int)(GetMasternodeRank(mn, pindexBest->nHeight)) << " " <<   // Rank
-                              (int64_t)mn.protocolVersion;                                  // Protocol Version of Masternode
+                              (string)(mn.IsEnabled() ? "Enabled" : "Error") << " " <<          // Is Enabled
+                              (int64_t)mn.lastTimeSeen << " " <<                                // Last time seen
+                              (int64_t)(mn.lastTimeSeen - mn.now) << " " <<                     // Time active (in sec)
+                              (int)(GetMasternodeRank(mn.vin, pindexBest->nHeight)) << " " <<   // Rank
+                              (int64_t)mn.protocolVersion;                                      // Protocol Version of Masternode
 
                 std::string strFull = streamFull.str();
                 obj.push_back(Pair(mn.addr.ToString().c_str(), strFull));
@@ -257,7 +257,7 @@ Value masternode(const Array& params, bool fHelp)
             } else if (strCommand == "activeseconds") {
                 obj.push_back(Pair(mn.addr.ToString().c_str(),       (int64_t)(mn.lastTimeSeen - mn.now)));
             } else if (strCommand == "rank") {
-                obj.push_back(Pair(mn.addr.ToString().c_str(),       (int)(GetMasternodeRank(mn, pindexBest->nHeight))));
+                obj.push_back(Pair(mn.addr.ToString().c_str(),       (int)(GetMasternodeRank(mn.vin, pindexBest->nHeight))));
             }
         }
         return obj;
