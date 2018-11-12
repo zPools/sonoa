5e41287c3a918ecce22f85f01fc7854a04b0d760
Mike <lpage8004@gmail.com>
2018-11-12 09:15:16 -0500

    Keccak replaced with SHA(MAY NOT BUILD)

--- src/fasthash.h
+++ src/fasthash.h
@@ -1,20 +1,20 @@
-// SHA3 (Keccak) for Masternode score calculation. It´s way faster than SonoA on CPU, so the load should be reduced
+// SHA for Masternode score calculation. It´s way faster than SonoA on CPU, so the load should be reduced
 #ifndef HASHFAST_H
 #define HASHFAST_H
 
 #include "uint256.h"
-#include "SonoA/sph_keccak.h"
+#include "SonoA/sph_sha2.h"
 
 template<typename T1>
 inline uint256 fasthash(const T1 pbegin, const T1 pend)
 {
-    sph_keccak256_context ctx_keccak;
+    sph_sha512_context ctx_sha2;
     static unsigned char pblank[1];
     uint256 hash;
 
-    sph_keccak256_init(&ctx_keccak);
-    sph_keccak256 (&ctx_keccak, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
-    sph_keccak256_close(&ctx_keccak, static_cast<void*>(&hash));
+    sph_sha512_init(&ctx_sha2);
+    sph_sha512 (&ctx_sha2, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
+    sph_sha512_close(&ctx_sha2, static_cast<void*>(&hash));
 
     return hash;
 }
