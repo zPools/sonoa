8e24aca8fcb6530a8b28e9a877ab62ffabbd549a
Mike <31688573+zPools@users.noreply.github.com>
2019-01-11 10:01:55 +0100

    fix double txid on 'getblock "blockhash" true'
    "entry.push_back(Pair("txid", tx.GetHash().GetHex()));" seems to be a leftover from a deprecated verbosity.
    Removing it fixes and removes the double txid in the result

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -132,7 +132,6 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool fPri
         {
             Object entry;
 
-            entry.push_back(Pair("txid", tx.GetHash().GetHex()));
             TxToJSON(tx, 0, entry);
 
             txinfo.push_back(entry);
